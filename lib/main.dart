be4f6de8a29a5887948d80a1544752e0da5650dc
Mahmoud <mahmoudabohebil@gmail.com>
2023-07-27 10:58:39 +0300

    The Application is finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:bmi_calculator/screens/results_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/input_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(BMICalculator());
 }
 
